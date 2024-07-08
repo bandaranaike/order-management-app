99ea90c638664e750bec394684fe00a38b52b9a0
Eranda Bandaranaike <ergroove@live.com>
2024-07-08 11:40:58 +0530

    Added README.md file and cleaned the api route

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 use App\Http\Controllers\OrderController;
 
 Route::post('/order', [OrderController::class, 'create'])->middleware('auth:sanctum');
-
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
-
-    return ['token' => $token->plainTextToken];
-});
